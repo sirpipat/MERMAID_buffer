cdad5c54eca7a62e754071e008c0d0d91c5a5e58
Sirawich Pipatprathanporn <sirpipat@umich.edu>
2019-11-11 14:47:22 -0500

    Fixed bug: not properly plot if the length is less than 1 hour

--- hourly_maker.m
+++ hourly_maker.m
@@ -1,4 +1,4 @@
-function hourly_maker(file, next)
+function [y,t_start,fs] = hourly_maker(file, next)
 % INPUT:
 % 
 % file      input file for making hourly plots
@@ -26,43 +26,57 @@ fs = length(y)/t_length;
 fprintf('size = %d, interval = %d, fs = %f\n', length(y), t_length, fs);
 
 % normalize the response
-y = y/max([max(y),abs(min(y))]);
+yn = y/max([max(y),abs(min(y))]);
 
 % filter the signal
-yf= bandpass(y,fs,fc(1,1),fc(1,2),2,2,'butter','linear');
+yf= bandpass(yn,fs,fc(1,1),fc(1,2),2,2,'butter','linear');
 
-% split to hourly plots
-% plot first hour
-t = 1;
-offset = 0;
+if t_length > 3600
+    % split to hourly plots
+    % plot first hour
+    t = 1;
+    offset = 0;
 
-% min(interval to next hour, total length): detect < 1hr length data
-dm = min(interval(t_start, find_nexthour(t_start)),t_length);
-hold on
-if dm ~= 0
-    hour_y = y(round(t*fs):round((t+dm-1)*fs));
-    hour_yf = yf(round(t*fs):round((t+dm-1)*fs));
-    plot_section(hour_y, hour_yf, fs, t_start, 3600 - dm, offset);
-    t = t + dm;
-    offset = offset + 2;
-end
+    % min(interval to next hour, total length): detect < 1hr length data
+    dm = min(interval(t_start, find_nexthour(t_start)),t_length);
+    hold on
+    if dm ~= 0
+        hour_y = yn(round(t*fs):round((t+dm-1)*fs));
+        hour_yf = yf(round(t*fs):round((t+dm-1)*fs));
+        plot_section(hour_y, hour_yf, fs, t_start, 3600 - dm, offset);
+        t = t + dm;
+        offset = offset + 2;
+    end
 
-% plot following hours
-while t < t_length
-    % slice y into 1-hour chunk
-    dt = min(3600, t_length - t);
-    hour_y = y(round(t*fs):round((t+dt)*fs));
-    hour_yf = yf(round(t*fs):round((t+dt)*fs));
-    plot_section(hour_y, hour_yf, fs, t_start, 0, offset);
-    t = t + 3600;
-    offset = offset + 2;
+    % plot following hours
+    while t < t_length
+        % slice y into 1-hour chunk
+        dt = min(3600, t_length - t);
+        hour_y = yn(round(t*fs):round((t+dt)*fs));
+        hour_yf = yf(round(t*fs):round((t+dt)*fs));
+        plot_section(hour_y, hour_yf, fs, t_start, 0, offset);
+        t = t + 3600;
+        offset = offset + 2;
+    end
+else
+    hold on
+    plot_section(yn, yf, fs, t_start, ...
+                 interval(find_previoushour(t_start), t_start), 0);
 end
 
 grid on
-xlabel('time')
+xlabel('time (mm:ss)')
 hold off
 set(gca, 'YDir','reverse')
-title(sprintf('%s',replace(filename,'_','\_')))
+title(sprintf('%s',replace(remove_path(filename),'_','\_')))
+end
+
+% remove the path from filename string
+% e.g. remove_path('/home/Document/file.txt') == 'file.txt'
+function filename = remove_path(full_filename)
+    % remove file path from the file name
+    splited_name = split(full_filename, '/');
+    filename = cell2mat(splited_name(end));
 end
 
 % Requires: time1 must be before time2
@@ -79,6 +93,7 @@ end
 % Modifies: nothing
 % Effects:  calculate datetime of the file
 function t = time(filename)
+    filename = remove_path(filename);
     datestr = replace(filename,'_',':');
     % append '.000000' if ss does not have any decimals.
     if ~contains(datestr,'.')
@@ -115,7 +130,13 @@ function t = find_previoushour(dtime)
     t = datetime(dvec);
 end
 
-
+% INPUT Parameters
+% y         = unfiltered signal
+% yf        = filtered signal
+% fs        = sampling rate
+% t_start   = the start time of the record
+% t_begin   = the start time of the section
+% offset    = the offset of the section
 function plot_section(y,yf,fs,t_start,t_begin,offset)
     t_start = find_previoushour(t_start);
     x = linspace(t_begin, t_begin + length(y)/fs , length(y));
