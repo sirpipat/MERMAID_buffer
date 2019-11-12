c30fe9aeba2ee2bc3a5eb606a1ce7635732ea09b
Sirawich Pipatprathanporn <sirpipat@umich.edu>
2019-11-11 22:56:30 -0500

    Changed x-axis to days and added manual

--- year_maker.m
+++ year_maker.m
@@ -1,4 +1,15 @@
 function year_maker(ddir, i_begin, i_end)
+% INPUT
+% ddir                       = directory of the one year data files
+% i_begin [min = 1]          = first index of files to included in the plot
+% i_end   [max = # of files] = last index of files to included in the plot
+%
+% If i_begin or i_end does not follow the min/max bound, it will be 
+% assumed to be the min/max value.
+%
+% Any calls of i_begin and i_end are valid as long as  i_begin >= i_end
+%
+% Last modified by Sirawich Pipatprathanporn, 11/11/2019
 
 % Assume sampling frequency to be 40 Hz
 fs = 40;
@@ -18,27 +29,33 @@ for index = max(1,i_begin):min(i_end,fndex)
     
     % plot each day section
     t_curr = t_start;
-    while t_end > find_next_midnight(t_curr)
-        x_begin = t_curr - find_prev_midnight(t_curr);
-        x_end = find_next_midnight(t_curr) - find_prev_midnight(t_curr);
+    while t_end > find_next_month(t_curr)
+        x_begin = t_curr - find_prev_month(t_curr) + 1;
+        x_begin.Format = 'd';
+        x_end = find_next_month(t_curr) - find_prev_month(t_curr) + 1;
+        x_end.Format = 'd';
         xx = [x_begin x_end];
-        y = find_prev_midnight(t_curr);
+        y = find_prev_month(t_curr);
         yy = [y y];
-        plot(xx, yy, 'k');
+        p = plot(xx, yy, 'k');
+        p.LineWidth = 2;
         % update
-        t_curr = find_next_midnight(t_curr);
+        t_curr = find_next_month(t_curr);
     end
     
     % plot last day section
-    x_begin = t_curr - find_prev_midnight(t_curr);
-    x_end = t_end - find_prev_midnight(t_curr);
+    x_begin = t_curr - find_prev_month(t_curr) + 1;
+    x_begin.Format = 'd';
+    x_end = t_end - find_prev_month(t_curr) + 1;
+    x_end.Format = 'd';
     xx = [x_begin x_end];
-    y = find_prev_midnight(t_curr);
+    y = find_prev_month(t_curr);
     yy = [y y];
-    plot(xx, yy,'k');
+    p = plot(xx, yy,'k');
+    p.LineWidth = 2;
 end
 grid on
-xlabel('time (mm:ss)')
+title('Year plot');
 set(gca, 'YDir','reverse')
 end
 
@@ -65,21 +82,39 @@ function t = time(filename)
     t = datetime(datestr,'InputFormat','yyyy-MM-dd''T''HH:mm:ss.SSSSSS');
 end
 
-% return the beginning of the midnight 
-function t = find_prev_midnight(dtime)
+% return the beginning of the month 
+function t = find_prev_month(dtime)
     dvec = datevec(dtime);
+    dvec(3) = 1;
     dvec(4) = 0;
     dvec(5) = 0;
     dvec(6) = 0;
     t = datetime(dvec);
 end
 
-% return the beginning of the next midnight 
-function t = find_next_midnight(dtime)
+% return the beginning of the next month 
+function t = find_next_month(dtime)
     dvec = datevec(dtime);
-    dvec(3) = dvec(3) + 1;
+    dvec(2) = dvec(2) + 1;
+    dvec(3) = 1;
     dvec(4) = 0;
     dvec(5) = 0;
     dvec(6) = 0;
     t = datetime(dvec);
+end
+
+% Requires: time1 must be before time2
+% Modifies: nothing
+% Effects:  find the interval between the two datetimes in days
+function dt = interval(time1,time2)
+    dur = time2 - time1;
+    dvec = datevec(dur);
+    dt = datevec_to_days(dvec);
+end
+
+% return time interval of dvec in seconds
+function dt = datevec_to_days(dvec)
+    dt = dvec(1) * 365.2425 + dvec(3);
+    dt = ((dt * 24 + dvec(4)) * 60 + dvec(5)) * 60 + dvec(6);
+    dt = dt / 86400;
 end
