098bb6afe13811d3140cd8737265f2062bed5e2a
Sirawich Pipatprathanporn <sirpipat@umich.edu>
2019-11-16 22:11:43 -0500

    Reverted back to plotting from 0 to t again

--- signalplot.m
+++ signalplot.m
@@ -1,11 +1,13 @@
 function signalplot(x, fs)
 
 L = length(x);
-t = (-L/2:L/2-1) / fs;
+t = (0:L-1) / fs;
+max_x = max([max(x) abs(min(x))]);
 
 plot(t, x);
 title('Signal');       
 xlabel('Time (s)')         
 ylabel('Response');
-
+xlim([0 (L-1)/fs]);
+ylim([-max_x max_x]);
 end
