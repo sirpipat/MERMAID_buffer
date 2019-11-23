5d8bc5320ccd141f112fb3eadecc4fb5d842cb9c
Sirawich Pipatprathanporn <sirpipat@umich.edu>
2019-11-23 15:13:39 -0500

    Change x-axis to datetime

--- signalplot.m
+++ signalplot.m
@@ -1,19 +1,30 @@
-function signalplot(x, fs, position)
+function signalplot(x, fs, t, position)
+% signalplot(x, fs, position)
+% Make a plot of the signal
+%
+% INPUT
+% x         = input signal
+% fs        = sampling frequency
+% position  = position of zero either left or center
+% Last modified by Sirawich Pipatprathanporn, 11/23/2019
 
 defval('position','left');
 
+% seconds to days
+s2d = 86400;
+
 L = length(x);
-t = (0:L-1) / fs;
+t_plot = t + (0:L-1) / fs / s2d;
 if strcmp(position, 'center') == 1
-    t = t - L/2/fs;
+    t_plot = t_plot - L/2/fs / s2d;
 end
 
 max_x = max([max(x) abs(min(x))]);
 
-plot(t, x);
+plot(t_plot, x);
 title('Signal');       
-xlabel('Time (s)')         
-ylabel('Response');
+xlabel('Time')
 ylim([-max_x max_x]);
-xlim([min(t) max(t)]);
+xlim([min(t_plot) max(t_plot)]);
+grid on
 end
