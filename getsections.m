2ce1cff0d41f2fe3a338d40033f5bae9df52ebdc
Sirawich Pipatprathanporn <sirpipat@umich.edu>
2020-01-23 20:32:53 -0500

    Tool to find MERMAID uptime sections

---
+++ getsections.m
@@ -0,0 +1,69 @@
+function [sections, intervals] = getsections(dir, dt_begin, dt_end, fs)
+% [sections, intervals] = getsections(dir, dt_begin, dt_end, fs)
+% Find the uptime sections of MERMAID P0023
+%
+% INPUT:
+% dir           Directory of the one year data [Default: $ONEYEAR]
+% dt_begin      Beginning datetime
+% dt_end        End datetime
+% fs            Sampling rate [Default: 40]
+%
+% OUTPUT:
+% sections      Cell containing full-filenames of the sections overlapping
+%               dt_begin to dt_end interval
+% intervals     Cell containing the beginning and end of each section
+%
+% SEE ALSO:
+% ONEYEARDATA, FILE2DATETIME, GETFNDEX, READONEYEARDATA
+%
+% Last modified by Sirawich Pipatprathanporn: 01/23/2020
+
+defval('dir', getenv('ONEYEAR'));
+defval('fs', 40);
+
+[allfiles, fndex] = oneyeardata(dir);
+
+% Gets all the start datetime of each file
+allbegins = cell(fndex);
+for ii = 1:fndex
+    allbegins{ii} = file2datetime(allfiles{ii});
+end
+
+% returns nothing if the input argument is invalid
+if ~is_dt_argument_valid(dt_begin, dt_end, allbegins{1})
+    sections = {};
+    intervals = {};
+    return
+end
+
+% finds the beginning section and the end section
+fndex_begin = getfndex(allbegins, dt_begin);
+fndex_end = getfndex(allbegins, dt_end);
+
+sections = allfiles(fndex_begin:fndex_end);
+intervals = {};
+
+for ii = fndex_begin:fndex_end
+    % index for intervals = 1:length(sections)
+    jj = ii - fndex_begin + 1;
+    % finds the end time of the file
+    [~, ~, dt_end] = readOneYearData(allfiles{ii}, fs);
+    interval_begin = max(dt_begin, allbegins{ii});
+    interval_end = min(dt_end, dt_end);
+    intervals{jj} = {interval_begin, interval_end};
+end
+
+end
+
+function is_valid = is_dt_argument_valid(dt_begin, dt_end, first_dt)
+    % if the one of following conditions is true, return false
+    is_valid = 0;
+    if dt_begin >= dt_end
+        return
+    end
+    if dt_end <= first_dt
+        return
+    end
+    % otherwise, return true
+    is_valid = 1;
+end
