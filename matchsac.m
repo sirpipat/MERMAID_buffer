25ed8b6c312a7f29d876df7918c894b5b4ebca9e
Sirawich Pipatprathanporn <sirpipat@umich.edu>
2020-02-06 23:37:56 -0500

    Matches SAC files with appropriate sections from OneYearData

--- matchsac.m
+++ matchsac.m
@@ -1,9 +1,11 @@
-function matchsac(sacfile, merdir)
+function matchsac(sacfile, merdir, savedir)
 % Finds a section in OneYearData that SAC file belongs to
+% Then plots xcorr and matched signals
 %
 % INPUT:
 % sacfile       Full filename of the sacfile
 % merdir        Directory of the MERMAID files [Default: $ONEYEAR]
+% savedir       Directory you wish to save the figures
 %
 % OUTPUT:
 % TBD
@@ -11,13 +13,16 @@ function matchsac(sacfile, merdir)
 % SEE ALSO:
 % READSAC, GETSECTIONS, READSECTION, 
 %
-% Last modified by Sirawich Pipatprathanporn, 02/05/2020
-
+% Last modified by Sirawich Pipatprathanporn, 02/06/2020
 
 defval('merdir', getenv('ONEYEAR'));
 
-% maximum t_shift in seconds
-max_t_shift = 1200;
+% file name for figures
+split_name = split(removepath(sacfile), '.');
+filename = cell2mat(split_name(1));
+
+% maximum margin from both end of SAC datetimes in seconds
+max_margin = 1200;
 
 % the number of seconds in a day
 d2s = 86400;
@@ -29,9 +34,13 @@ dt_ref = datetime(Hdr.NZYEAR, 1, 0, Hdr.NZHOUR, Hdr.NZMIN, Hdr.NZSEC, ...
 dt_B = dt_ref + Hdr.B / d2s;
 dt_E = dt_ref + Hdr.E / d2s;
 
+fprintf("Reported section: %s -- %s\n", string(dt_B), string(dt_E));
+
 % finds MERMAID file(s) containing dt_B and dt_E
-[sections, intervals] = getsections(merdir, dt_B - max_t_shift / d2s, ...
-    dt_E + max_t_shift / d2s);
+[sections, intervals] = getsections(merdir, dt_B - max_margin / d2s, ...
+    dt_E + max_margin / d2s);
+% update max_margin
+max_margin = seconds(dt_B - intervals{1}{1});
 
 % reads the section from MERMAID file(s)
 % Assuming there is only 1 secion
@@ -40,42 +49,50 @@ dt_E = dt_ref + Hdr.E / d2s;
 
 % decimates to obtain sampling rate about 10 Hz
 fs = 10;
-x_sac = decimate(x_sac, 2);
-x_mer = decimate(x_mer, 4);
+x_sacd = decimate(x_sac, 2);
+x_merd = decimate(x_mer, 4);
 
 % applies Butterworth bandpass 0.05-0.10 Hz
-x_sacf = bandpass(x_sac, fs, 0.05, 0.10, 2, 1, 'butter', 'linear');
-x_merf = bandpass(x_mer, fs, 0.05, 0.10, 2, 1, 'butter', 'linear');
+x_sacf = bandpass(x_sacd, fs, 0.05, 0.10, 2, 1, 'butter', 'linear');
+x_merf = bandpass(x_merd, fs, 0.05, 0.10, 2, 1, 'butter', 'linear');
 
-% finds timeshift
-C = xcorr(x_merf, x_sacf);
+% finds timeshift for raw SAC signal
+C = xcorr(x_merd, x_sacd);
 [Cmax, Imax] = max(C);
+t_shift = ((Imax - length(x_merd)) / 10) - max_margin;
+I = ((1:length(C)) - length(x_merd)) / 10 - max_margin;
 figure(1)
-plot(C);
-t_shift = ((Imax - length(x_merf)) / 10) - max_t_shift;
-fprintf('shifted time = %f s\n', t_shift);
-
-figure(2);
-signalplot(x_sacf, fs, dt_B + t_shift / d2s);
+plot(I, C);
 grid on
-title('xcorr')
-hold on
-signalplot(x_merf, fs, dt_begin);
-grid on
-hold off
-title('Filtered signals');
-fig = gcf();
-ax = fig.Children;
-ax.Children(2).YData = ax.Children(2).YData - 2 * max(ax.Children(1).YData);
-ax.YLim = max(ax.Children(1).YData) * [-3 1];
-ax.XLim = [dt_begin dt_end];
+title('XCORR [unfiltered]');
+xlabel('time shift [s]');
+ylabel('XCORR');
+savefile = strcat(savedir, filename, '_xcorr_raw.eps');
+saveas(gcf, savefile, 'epsc');
 
+fprintf('shifted time [RAW]      = %f s\n', t_shift);
+
+% find timeshift for filtered SAC signal
+Cf = xcorr(x_merf, x_sacf);
+[Cmaxf, Imaxf] = max(Cf);
+t_shiftf = ((Imaxf - length(x_merf)) / 10) - max_margin;
+I = ((1:length(C)) - length(x_merd)) / 10 - max_margin;
+figure(2)
+plot(I, Cf);
+grid on
+title('XCORR [filtered, 0.05-0.10 Hz]');
+xlabel('time shift [s]');
+ylabel('XCORR');
+savefile = strcat(savedir, filename, '_xcorr_fil.eps');
+saveas(gcf, savefile, 'epsc');
+
+fprintf('shifted time [FILTERED] = %f s\n', t_shiftf);
+
+% plot raw signals
 figure(3);
-signalplot(x_sac, fs, dt_B + t_shift / d2s);
-grid on
-title('xcorr')
+signalplot(x_sacd, fs, dt_B + t_shift / d2s);
 hold on
-signalplot(x_mer, fs, dt_begin);
+signalplot(x_merd, fs, dt_begin);
 grid on
 hold off
 title('Unfiltered signals');
@@ -84,4 +101,24 @@ ax = fig.Children;
 ax.Children(2).YData = ax.Children(2).YData - 2 * max(ax.Children(1).YData);
 ax.YLim = max(ax.Children(1).YData) * [-3 1];
 ax.XLim = [dt_begin dt_end];
+legend('SAC', 'MER');
+savefile = strcat(savedir, filename, '_match_raw.eps');
+saveas(gcf, savefile, 'epsc');
+
+% plot filtered signals
+figure(4);
+signalplot(x_sacf, fs, dt_B + t_shiftf / d2s);
+grid on
+hold on
+signalplot(x_merf, fs, dt_begin);
+hold off
+title('Filtered [0.05-0.10 Hz] signals');
+fig = gcf();
+ax = fig.Children;
+ax.Children(2).YData = ax.Children(2).YData - 2 * max(ax.Children(1).YData);
+ax.YLim = max(ax.Children(1).YData) * [-3 1];
+ax.XLim = [dt_begin dt_end];
+legend('SAC', 'MER');
+savefile = strcat(savedir, filename, '_match_fil.eps');
+saveas(gcf, savefile, 'epsc');
 end
