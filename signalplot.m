550df1628d540f059fa8cde43e01134cdb8cf250
Sirawich Pipatprathanporn <sirpipat@umich.edu>
2020-03-08 15:34:18 -0400

    Added linestyle input argument

--- signalplot.m
+++ signalplot.m
@@ -1,4 +1,4 @@
-function ax = signalplot(x, fs, t, ax, title_name, position)
+function ax = signalplot(x, fs, t, ax, title_name, position, color)
 % ax = SIGNALPLOT(x, fs, t, ax, title_name, position)
 % Make a plot of the signal
 %
@@ -9,14 +9,16 @@ function ax = signalplot(x, fs, t, ax, title_name, position)
 % ax        = current axes
 % title     = title of the plot
 % position  = position of zero either left or center
+% color     = the color of the line
 %
 % OUTPUT
 % ax        = handling axes of the plot
 %
-% Last modified by Sirawich Pipatprathanporn, 02/07/2020
+% Last modified by Sirawich Pipatprathanporn, 03/07/2020
 
 defval('title_name', 'signal');
 defval('position', 'left');
+defval('color', 'black');
 
 % seconds to days
 s2d = 86400;
@@ -31,7 +33,7 @@ max_x = max([max(x) abs(min(x))]);
 
 % plots the signal
 axes(ax);
-plot(t_plot, x);
+plot(t_plot, x,'Color',color);
 title('Signal');       
 xlabel('Time')
 ylim([-max_x max_x]);
