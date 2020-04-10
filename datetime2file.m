fda80535c3ad83876775d8aef00bdaa18e017597
sirpipat <sirpipat@umich.edu>
2020-04-10 19:07:03 -0400

    Convert datetime to filename in the same format as MERMAID raw buffer files

---
+++ datetime2file.m
@@ -0,0 +1,17 @@
+function filename = datetime2file(dt)
+% filename = TIME2DATETIME(dt)
+%
+% convert the datetime to the filename with the following format:
+% 'uuuu-MM-ddTHH:mm:ss.SSSSSS', the same as raw buffer file from MERMAID
+%
+% INPUT:
+% dt            datetime
+% 
+% OUTPUT:
+% filename      the output filename
+%
+% Last modified by Sirawich Pipatprathanporn: 04/10/2020
+
+dt.format = 'uuuu-MM-dd''T''HH:mm:ss.SSSSSS';
+filename  = string(dt);
+end
