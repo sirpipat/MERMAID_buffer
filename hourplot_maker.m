cadd4114abbcab784929a06581cc5df2ec19ab76
sirpipat <sirpipat@umich.edu>
2020-04-11 16:25:37 -0400

    Used figdisp to save the figure

--- hourplot_maker.m
+++ hourplot_maker.m
@@ -12,7 +12,7 @@ function hourplot_maker(hscdir, savedir)
 %
 %  No output besides the hourplots
 %
-% Last modified by Sirawich Pipatprathanporn, 12/16/2019
+% Last modified by Sirawich Pipatprathanporn, 04/11/2020
 
 [allhscfiles, hndex] = allfile(hscdir);
 
@@ -24,39 +24,17 @@ for ii = 1:hndex
     % read the files
     y = loadb(allhscfiles{ii},'int32','l');
     
-    signalplot(y,fs, time(erase(allhscfiles{ii},'.hsc')), 'left');
+    signalplot(y, fs, file2datetime(erase(allhscfiles{ii},'.hsc')), ...
+        axes, [], 'left');
     
     savefile = erase(allhscfiles{ii},'.hsc');
-    savefile = remove_path(savefile);
+    savefile = removepath(savefile);
     % includes datetime to the title of the plot
     titlestr = replace(savefile, '_','\_');
     title(titlestr);
 
     % save the figure
-    savefile = strcat(savedir, savefile, '_rawplot', '.eps');
-    saveas(gcf, savefile,'epsc');
+    savefile = strcat(savedir, savefile, '_rawplot');
+    figdisp(savefile, [], [], 2, [], 'esptopdf');
 end
-end
-
-% Requires: filename must be in this format: 
-%           "yyyy-MM-dd 'T' HH:mm:SS(.ssssss)"
-% Modifies: nothing
-% Effects:  calculate datetime of the file
-function t = time(filename)
-    filename = remove_path(filename);
-    datestr = replace(filename,'_',':');
-    % append '.000000' if ss does not have any decimals.
-    if ~contains(datestr,'.')
-        str = [datestr,'.000000'];
-        datestr = join(str);
-    end
-    t = datetime(datestr,'InputFormat','yyyy-MM-dd''T''HH:mm:ss.SSSSSS');
-end
-
-% remove the path from filename string
-% e.g. remove_path('/home/Document/file.txt') == 'file.txt'
-function filename = remove_path(full_filename)
-    % remove file path from the file name
-    splited_name = split(full_filename, '/');
-    filename = cell2mat(splited_name(end));
 end
