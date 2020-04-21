1854f94a2d20e25559ce30e7569f75819f2054e0
sirpipat <sirpipat@umich.edu>
2020-04-20 22:11:00 -0400

    From now on, it will read raw signal instead of a file.

--- plot_all.m
+++ plot_all.m
@@ -1,91 +1,143 @@
-function plot_all(filename)
+function plot_all(y, dt_begin, nfft, fs, lwin, olap, sfax, beg, unit)
+% PLOT_ALL(y, dt_begin, nfft, fs, lwin, olap, sfax, beg, unit)
 % plot seismogram, power spectral density, spectograms, and filtered
 % seismogram
+% 
+% INPUT
+% y             Data
+% dt_begin      Beginning datetime
+% nfft          Number of FFT points [default: lwin]
+% fs            Sampling frequency [Default: 40.01406]
+% lwin          Window length, in samples [default: 256]
+% olap          Window overlap, in percent [default: 70]
+% sfax          Y-axis scaling factor [default: 10]
+% beg           Signal beginning [Default: 0]
+% unit          String with the unit name [Default: 's']
+%
+% OUTPUT
+% No output returned. The plot is saved in 
+% 
+% Last modified by Sirawich Pipatprathanporn: 04/20/2020
 
 % parameter list
-fs = 40;
-s2d = 86400;
-nfft = 1024;
+defval('fs', 40.01406);
+defval('nfft', 1024);
+defval('lwin', 1024);
+defval('olap', 70);
+defval('sfax', 10);
+defval('beg', 0);
+defval('unit', 's');
+wolap = olap / 100;
 
-% for specsdensplot
-wlen = 1024;
-wolap = 0.70;
-beg = 0;
+dt_begin.Format = 'uuuu-MM-dd''T''HH:mm:ss.SSSSSS';
 
-% for timspecplot_ns
-lwin = 1024;
-olap = 70;
-sfax = 10;
+% filters with f_c = [2 10] Hz
+yf1 = bandpass(y, fs, 2, 10, 2, 2, 'butter', 'linear');
 
-% reads data
-y = loadb(filename, 'int32', 'l');
+% filters with f_c = [0.05 0.10] Hz
+d_factor = 5;
+yd = detrend(decimate(y, d_factor),1);
+yf2 = bandpass(yd, fs/d_factor, 0.05, 0.10, 2, 2, 'butter', 'linear');
 
-% filters with f_c = [0.75 2] Hz
-yf = bandpass(y, fs, 0.75, 2, 2, 2, 'butter', 'linear');
+%% Create figure
+figure(2)
+set(gcf, 'Unit', 'inches', 'Position', [18 8 6.5 7.5]);
+clf
 
-% calculates time for plotting variables in time-domain 
-filename = erase(filename,'.hsc');
-t_start = time(filename);
-t = t_start + 1/fs/s2d * (1:length(y));
+% plot title
+ax0 = subplot('Position',[0.05 0.93 0.9 0.02]);
+title(string(dt_begin));
+set(ax0, 'FontSize', 12, 'Color', 'none');
+ax0.XAxis.Visible = 'off';
+ax0.YAxis.Visible = 'off';
 
-%%% plot raw signal
-subplot(2, 2, 1);
-plot(t, y);
-grid on
-xlim([min(t) max(t)]);
+%%% plot spectogram
+ax1 = subplot('Position', [0.06 4/7 0.43 3/7-0.12]);
+timspecplot_ns(y,nfft,fs,lwin,wolap,beg,unit);
+title('');
+% insert colorbar
+c = colorbar;
+c.Label.String = 'spectral density (energy/Hz)';
+% change xticks to HH:mm and date
+t_ph = previous_hour(dt_begin);
+ax1.XTick = (0:900:7200) - seconds(dt_begin - t_ph);
+t_ph.Format = 'HH:mm';
+ax1.XTickLabel = string(t_ph + seconds(0:900:7200));
+% add label on the top and right
+ax1.TickDir = 'both';
+ax1s = doubleaxes(ax1);
+ax1s.YAxis.Visible = 'off';
 % add subplot label
-text(datetime(2019, 1, 20, 1, 13, 0), 5000000, 'a', 'FontSize', 14);
+[x_pos, y_pos] = norm2trueposition(ax1, 1/8, 7/8);
+text(x_pos, y_pos, 'A', 'FontSize', 12);
 
 %%% plot power spectral density profile
-subplot(2, 2, 2);
-[p,xl,yl,F,SD,Ulog,Llog]=specdensplot(y,nfft,fs,lwin,olap,sfax,'s');
+ax2 = subplot('Position', [0.61 4/7 0.33 3/7-0.12]);
+[p,xl,yl,F,SD,Ulog,Llog]=specdensplot(y,nfft,fs,lwin,olap,sfax,unit);
 grid on
 ylim([40 140]);
 % change color line
 p(1).Color = [0.8 0.25 0.25];
 p(2).Color = [0.5 0.5 0.5];
 p(3).Color = [0.5 0.5 0.5];
+% fix the precision of the frequency on YAxis label
+y_label = ax2.YAxis.Label.String;
+y_split = split(y_label, '=');
+f_string = sprintf(' %.4f', fs/nfft);
+y_label = strcat(y_split{1}, ' =', f_string);
+ax2.YAxis.Label.String = y_label;
+% add label on the top and right
+ax2.TickDir = 'both';
+ax2s = doubleaxes(ax2);
+% add axis label
+inverseaxis(ax2s.XAxis, 'Period (s)');
 % add subplot label
-text(0.05, 130, 'b', 'FontSize', 14);
+[x_pos, y_pos] = norm2trueposition(ax2, 1/800, 7/8);
+text(x_pos, y_pos, 'B', 'FontSize', 12);
 
-%%% plot spectogram
-subplot(2, 2, 3);
-timspecplot_ns(y,nfft,fs,wlen,wolap,beg,'s');
-title('');
-% insert colorbar
-c = colorbar;
-c.Label.String = 'spectral density (energy/Hz)';
+%%% plot raw signal
+ax3 = subplot('Position', [0.06 2/6+0.06 0.88 1/6-0.09]);
+ax3 = signalplot(y, fs, dt_begin, ax3, '', 'left');
+title('Raw buffer')
+ax3.TickDir = 'both';
+% set ylimit to exclude outliers
+r = rms(y);
+ylim([-5*r 5*r]);
 % add subplot label
-text(150, 18, 'c', 'FontSize', 14);
+[x_pos, y_pos] = norm2trueposition(ax3, 1/12, 7/8);
+text(x_pos, y_pos, 'C', 'FontSize', 12);
+
+%%% plot raw signal
+ax4 = subplot('Position', [0.06 1/6+0.06 0.88 1/6-0.09]);
+ax4 = signalplot(yf1, fs, dt_begin, ax4, '', 'left');
+title('Filtered: bp2-10')
+ax4.TickDir = 'both';
+% set ylimit to exclude outliers
+r = rms(yf1);
+ylim([-10*r 10*r]);
+% add subplot label
+[x_pos, y_pos] = norm2trueposition(ax4, 1/12, 7/8);
+text(x_pos, y_pos, 'D', 'FontSize', 12);
 
 %%% plot filtered signal
-subplot(2, 2, 4);
-plot(t, yf);
-grid on
-xlim([min(t) max(t)]);
+ax5 = subplot('Position', [0.06 0.06 0.88 1/6-0.09]);
+ax5 = signalplot(yf2, fs/d_factor, dt_begin, ax5, '', 'left');
+title('Filtered: dc5 dt bp0.05-0.1')
+ax5.TickDir = 'both';
+% set ylimit to exclude outliers
+r = rms(yf2);
+ylim([-5*r 5*r]);
 % add subplot label
-text(datetime(2019, 1, 20, 1, 13, 0), 500000, 'd', 'FontSize', 14);
+[x_pos, y_pos] = norm2trueposition(ax5, 1/12, 7/8);
+text(x_pos, y_pos, 'E', 'FontSize', 12);
+
+%% Save figure
+savefile = strcat(string(dt_begin), '_all', '.eps');
+figdisp(savefile, [], [], 2, [], 'epstopdf');
 end
 
-% Requires: filename must be in this format: 
-%           "yyyy-MM-dd 'T' HH:mm:SS(.ssssss)"
-% Modifies: nothing
-% Effects:  calculate datetime of the file
-function t = time(filename)
-    filename = remove_path(filename);
-    datestr = replace(filename,'_',':');
-    % append '.000000' if ss does not have any decimals.
-    if ~contains(datestr,'.')
-        str = [datestr,'.000000'];
-        datestr = join(str);
-    end
-    t = datetime(datestr,'InputFormat','yyyy-MM-dd''T''HH:mm:ss.SSSSSS');
-end
-
-% remove the path from filename string
-% e.g. remove_path('/home/Document/file.txt') == 'file.txt'
-function filename = remove_path(full_filename)
-    % remove file path from the file name
-    splited_name = split(full_filename, '/');
-    filename = cell2mat(splited_name(end));
+function ph = previous_hour(dt)
+    ph = dt;
+    ph.Minute = 0;
+    ph.Second = 0;
 end
