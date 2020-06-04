c3ef2c2b9bd0038d865eb8840ba0ea8e73e23e40
Sirawich Pipatprathanporn <sirawich@princeton.edu>
2020-06-03 21:25:51 -0400

    Renamed timeccplot.m to timccplot.m

--- timccplot.m
+++ timccplot.m
@@ -1,4 +1,4 @@
-function timeccplot(plotfilter)
+function timccplot(plotfilter)
 %% Read all SAC files
 sacdir = getenv('SAC');
 [allsacfiles, sndex] = sacdata(sacdir);
@@ -33,15 +33,21 @@ end
 %%
 % the time since dt_begin to the beginning time of the SAC segment
 t_since = hours((dt_B - dt_begin) + seconds(t_shift));
-t_sincef = hours((dt_B - dt_begin) + seconds(t_shiftf));
+if plotfilter
+    t_sincef = hours((dt_B - dt_begin) + seconds(t_shiftf));
+end
 
 % remove unmatched records
-where = and(abs(t_shiftf) < 200, abs(t_shift) < 200);
+where = abs(t_shift) < 200;
+if plotfilter
+    where = and(abs(t_shiftf) < 200, where);
+end
 t_since = t_since(where);
-t_sincef = t_sincef(where);
 t_shift = t_shift(where);
-t_shiftf = t_shiftf(where);
-
+if plotfilter
+    t_sincef = t_sincef(where);
+    t_shiftf = t_shiftf(where);
+end
 dt_begin = dt_begin(where);
 
 % find all unique dt_begin
@@ -50,23 +56,30 @@ dt_begin_label = string(1:length(dt_bu));
 
 % find best fit lines
 P_t_shift = polyfit(t_since, t_shift, 1);
-P_t_shiftf = polyfit(t_sincef, t_shiftf, 1);
-
 P_Cmax = polyfit(t_since, Cmax(where), 1);
-P_Cfmax = polyfit(t_sincef, Cfmax(where), 1);
+
+if plotfilter
+    P_t_shiftf = polyfit(t_sincef, t_shiftf, 1);
+    P_Cfmax = polyfit(t_sincef, Cfmax(where), 1);
+end
 
 figure(7)
+clf
 set(gcf,'Units','inches','Position',[1 1 9 4]);
 subplot('Position', [0.06 0.11 0.43 0.8]);
 text(t_since, t_shift, dt_begin_label(idt_bu));
 hold on
-p2 = scatter(t_sincef, t_shiftf, 'Marker', '+');
+if plotfilter
+    p2 = scatter(t_sincef, t_shiftf, 'Marker', '+');
+end
 p3 = plot(t_since, P_t_shift(1) * t_since + P_t_shift(2));
-p4 = plot(t_sincef, P_t_shiftf(1) * t_sincef + P_t_shiftf(2));
+P_label = sprintf('t-shfit [Raw] = (%6.4f) H + (%6.4f)', P_t_shift(1), P_t_shift(2));
+if plotfilter
+    p4 = plot(t_sincef, P_t_shiftf(1) * t_sincef + P_t_shiftf(2));
+    Pf_label = sprintf('t-shift [Filtered] = (%6.4f) H + (%6.4f)', P_t_shiftf(1), P_t_shiftf(2));
+end
 hold off
 grid on
-P_label = sprintf('t-shfit [Raw] = (%6.4f) H + (%6.4f)', P_t_shift(1), P_t_shift(2));
-Pf_label = sprintf('t-shift [Filtered] = (%6.4f) H + (%6.4f)', P_t_shiftf(1), P_t_shiftf(2));
 % legend('Time shift [Raw]','Time shift [Filtered]',P_label,Pf_label,...
 %     'Location','northwest');
 ylim([-0.1 0.5]);
@@ -74,36 +87,50 @@ xlim([0 140]);
 xlabel('Hours since the beginning of raw data section [hours]');
 ylabel('Time shift [s]');
 title('Time shift vs Hours since the beginning');
-delete(p2);
 delete(p3);
-delete(p4);
+if plotfilter
+    delete(p2);
+    delete(p4);
+end
 
 subplot('Position', [0.6 0.11 0.38 0.8]);
 p5 = scatter(t_since, Cmax(where), 'Marker', 'd', 'MarkerEdgeColor', 'k', ...
     'MarkerFaceColor', 'b');
 hold on
-p6 = scatter(t_sincef, Cfmax(where), 'Marker', '+');
+if plotfilter
+    p6 = scatter(t_sincef, Cfmax(where), 'Marker', '+');
+end
 p7 = plot(t_since, P_Cmax(1) * t_since + P_Cmax(2));
-p8 = plot(t_sincef, P_Cfmax(1) * t_sincef + P_Cfmax(2));
+P_label = sprintf('CC [Raw] = (%6.4f) H + (%6.4f)', P_Cmax(1), P_Cmax(2));
+if plotfilter
+    p8 = plot(t_sincef, P_Cfmax(1) * t_sincef + P_Cfmax(2));
+    Pf_label = sprintf('CC [Filtered] = (%6.4f) H + (%6.4f)', P_Cfmax(1), P_Cfmax(2));
+end
 hold off
 grid on
-P_label = sprintf('CC [Raw] = (%6.4f) H + (%6.4f)', P_Cmax(1), P_Cmax(2));
-Pf_label = sprintf('CC [Filtered] = (%6.4f) H + (%6.4f)', P_Cfmax(1), P_Cfmax(2));
-legend('CC [Raw]','CC [Filtered]',P_label,Pf_label,'Location','southwest');
+% legend('CC [Raw]','CC [Filtered]',P_label,Pf_label,'Location','southwest');
 ylim([0.998 1.0005]);
 xlabel('Hours since the beginning of raw data section [hours]');
 ylabel('Correlation coefficients');
-delete(p6);
 delete(p7);
-delete(p8);
+if plotfilter
+    delete(p6);
+    delete(p8);
+end
 
 title('CC vs Hours since the beginning');
 figdisp('timeshift_vs_hours_since_beginning', [], [], 2, [], 'epstopdf');
 
 %% plots histogram of CC [raw] next to histogram of CC [filtered]
 figure(8)
-set(gcf,'Units','inches','Position',[1 1 9 4]);
-subplot('Position', [0.06 0.11 0.43 0.8]);
+if plotfilter
+    set(gcf,'Units','inches','Position',[1 1 9 4]);
+    subplot('Position', [0.06 0.11 0.43 0.8]);
+else
+    set(gcf,'Units','inches','Position',[1 1 4.5 4]);
+    subplot('Position', [0.06 0.11 0.93 0.8]);
+end
+
 h = histogram(Cmax(where));
 h.BinWidth = 0.0005;
 grid on
@@ -112,14 +139,16 @@ ylabel('Counts');
 title_name = strcat('Histogram of CC [raw buffer, raw sac report]');
 title(title_name);
 
-subplot('Position', [0.55 0.11 0.43 0.8]);
-h = histogram(Cfmax(where));
-h.BinWidth = 0.05;
-grid on
-xlabel('Correlation coefficient');
-ylabel('Counts');
-title_name = strcat('Histogram of CC [filtered buffer, filtered sac report]');
-title(title_name);
+if plotfilter
+    subplot('Position', [0.55 0.11 0.43 0.8]);
+    h = histogram(Cfmax(where));
+    h.BinWidth = 0.05;
+    grid on
+    xlabel('Correlation coefficient');
+    ylabel('Counts');
+    title_name = strcat('Histogram of CC [filtered buffer, filtered sac report]');
+    title(title_name);
+end
 figdisp('cc_histograms', [], [], 2, [], 'epstopdf');
 
 end
