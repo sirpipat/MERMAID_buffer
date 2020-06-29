a9c8580b19067ec17891fe8067e32ed07e9dbe11
Sirawich Pipatprathanporn <sirawich@princeton.edu>
2020-06-29 16:20:28 +0700

    Enable plotting several lines at the same time

--- vline.m
+++ vline.m
@@ -12,7 +12,7 @@ function ax = vline(ax,x,linestyle,linewidth,linecolor)
 % OUTPUT:
 % ax            The handling axes of the vertical line
 %
-% Last modified by Sirawich Pipatprathanporn, 03/07/2020
+% Last modified by Sirawich Pipatprathanporn, 06/29/2020
 
 defval('linestyle','-');
 defval('linewidth',0.5);
@@ -20,7 +20,7 @@ defval('linecolor','r');
 defval('label','');
 
 axes(ax)
-v = plot([x x], [ax.YLim(1) ax.YLim(2)], 'LineStyle', linestyle, ...
+v = plot([x; x], [ax.YLim(1) ax.YLim(2)], 'LineStyle', linestyle, ...
     'LineWidth', linewidth, 'Color', linecolor);
 % this last part is so that it doesn't show up on legends
 set(v,'tag','vline','handlevisibility','off');
