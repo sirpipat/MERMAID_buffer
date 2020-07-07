4802912f203b25bc1713d8dac954a122294a82ae
Sirawich Pipatprathanporn <sirawich@princeton.edu>
2020-07-07 19:04:52 +0700

    Adds a box into a figure

---
+++ addbox.m
@@ -0,0 +1,40 @@
+function axb = addbox(ax,norm_position)
+% axb = ADDBOX(ax, norm_position)
+% Adds a box into a plot. It makes an axes handling the box the current
+% axes.
+%
+% INPUT
+% ax                axes to put a box inside
+% norm_position     normalized position of the box inside the axes
+%                   [left bottom width height]
+%
+% OUTPUT
+% axb               axes handling the box
+%
+% Last modified by Sirawich Pipatprathanporn: 07/07/2020
+
+% create an empty box
+axb = axes();
+
+left = ax.Position(1);
+bot = ax.Position(2);
+width = ax.Position(3);
+height = ax.Position(4);
+
+b_left = norm_position(1);
+b_bot = norm_position(2);
+b_width = norm_position(3);
+b_height = norm_position(4);
+
+% resize the box
+axb.Position = [left + b_left * width, ...
+                bot + b_bot * height, ...
+                b_width * width, ...
+                b_height * height];
+
+% create the box's boundary
+axb.Box = 'on';
+axb.BoxStyle = 'full';
+axb.XTick = [];
+axb.YTick = [];
+end
