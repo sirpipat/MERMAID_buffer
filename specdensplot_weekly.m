0d54a68c3c8173cc06ebd32914fb30580f76a9c3
Sirawich Pipatprathanporn <sirawich@princeton.edu>
2020-07-29 04:04:33 +0700

    Also saved SD profiles for later uses

--- specdensplot_weekly.m
+++ specdensplot_weekly.m
@@ -1,18 +1,20 @@
-function specdensplot_weekly()
+function specdensplot_weekly(win)
 % SPECDENSPLOT_WEEKLY()
 % makes spectral density of each week from Sep 2018 to Aug 2019 of ocean
 % noise recorded by a MERMAID P023
 %
 % INPUT
-% no input
+% win       Length of the window in seconds
 %
 % OUTPUT
-% no output beside figures saved at $EPS
+% figures saved at $EPS and
+% SD profiles saved at ...
+% /Users/sirawich/research/processed_data/weekly_SD_profiles/
 %
 % SEE ALSO:
 % SPECDENSPLOT_SECTION
 % 
-% Last modified by Sirawich Pipatprathanporn: 07/06/2020
+% Last modified by Sirawich Pipatprathanporn: 07/29/2020
 
 % dt_begin and dt_end for specdensplot_section
 dt_0 = datetime(2018, 9, 13, 'TimeZone', 'UTC', 'Format', ...
@@ -22,21 +24,32 @@ dt = dt_0 + calweeks(0:49);
 % input parameters for specdensplot_section
 excdir = '/Users/sirawich/research/processed_data/tphases/';
 fs = 40.01406;
-nfft = round(100 * fs);
+nfft = round(win * fs);
 lwin = nfft;
 olap = 70;
 sfax = 10;
 midval = 'median';
 method = 'pct';
-scale = 'log';
+scale = 'linear';
 plt = true;
 
+savedir = '/Users/sirawich/research/processed_data/weekly_SD_profiles/';
+
 for ii = 1:49
-    specdensplot_section(dt(ii), dt(ii+1), excdir, nfft, fs, lwin, ...
+    [~, ~, ~, F, ~, ~, Swmid, Swstd, SwU, SwL] = ...
+        specdensplot_section(dt(ii), dt(ii+1), excdir, nfft, fs, lwin, ...
         olap, sfax, midval, method, scale, plt);
     % save figure
     savefile = strcat(mfilename, '_', replace(string(dt(ii)), ':', ...
         '_'),'.eps');
     figdisp(savefile, [], [], 2, [], 'epstopdf');
+    
+    % write data
+    titles = sprintf('%d_%02d_%02d',dt(ii).Year,dt(ii).Month,dt(ii).Day);
+    fid = fopen(strcat(savedir,mfilename,'_',titles,'.txt'),'w');
+    % the columns: F     median   std   SwU(95%)     SwL(5%)
+    data = [F, Swmid, Swstd, SwU, SwL]';
+    fprintf(fid, '%10.6f %10.6f %10.6f %10.6f %10.6f\n', data);
+    fclose(fid);
 end
 end
