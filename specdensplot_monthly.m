fc8c02d0be4e86f78b84fad89a1e2b9eb6cb33ff
Sirawich Pipatprathanporn <sirawich@princeton.edu>
2020-08-03 22:46:58 +0700

    Added options to eigher 'plot' or 'save' spectral density to files

--- specdensplot_monthly.m
+++ specdensplot_monthly.m
@@ -1,10 +1,14 @@
-function specdensplot_monthly()
-% SPECDENSPLOT_MONTHLY()
+function specdensplot_monthly(win,option)
+% SPECDENSPLOT_MONTHLY(win,option)
 % makes spectral density of each month from Sep 2018 to Aug 2019 of ocean
 % noise recorded by a MERMAID P023
 %
 % INPUT
-% no input
+% win       Length of the window in seconds
+% option    'plot' or 'save'
+%           'plot' gives log-scale spectral density heat map
+%           'save' gives SD profiles saved at ...
+%           /Users/sirawich/research/processed_data/monthly_SD_profiles/
 %
 % OUTPUT
 % figures saved at $EPS and
@@ -12,9 +16,19 @@ function specdensplot_monthly()
 % /Users/sirawich/research/processed_data/monthly_SD_profiles/
 %
 % SEE ALSO:
-% SPECDENSPLOT_SECTION
+% SPECDENSPLOT_SECTION, SPECDENSPLOT_HEATMAP
 % 
-% Last modified by Sirawich Pipatprathanporn: 07/29/2020
+% Last modified by Sirawich Pipatprathanporn: 08/03/2020
+
+defval('win', 100)
+defval('option', 'plot')
+
+% check invalid input
+if win <= 0
+    error('win must be above 0.');
+elseif ~or(strcmp(option, 'plot'), strcmp(option, 'save'))
+    error('option must be either ''plot'' or ''save''');
+end
 
 % dt_begin and dt_end for specdensplot_section
 dt_0 = datetime(2018, 9, 1, 'TimeZone', 'UTC', 'Format', ...
@@ -30,8 +44,13 @@ olap = 70;
 sfax = 10;
 midval = 'median';
 method = 'pct';
-scale = 'linear';
-plt = true;
+if strcmp(option, 'save')
+    scale = 'log';
+    plt = false;
+else
+    scale = 'linear';
+    plt = true;
+end
 
 savedir = '/Users/sirawich/research/processed_data/monthly_SD_profiles/';
 % titles of spectral density profile output
@@ -40,18 +59,23 @@ titles = {'2018_09', '2018_10', '2018_11', '2018_12', '2019_01', ...
           '2019_07', '2019_08'};
 
 for ii = 1:12
-    [~,~,~,F,~,~,Swmid,Swstd,SwU,SwL] = specdensplot_section(dt(ii), dt(ii+1), excdir, nfft, fs, lwin, ...
-        olap, sfax, midval, method, scale, plt);
+    [~,~,~,F,~,~,Swmid,Swstd,SwU,SwL] = specdensplot_section(dt(ii), ...
+        dt(ii+1), excdir, nfft, fs, lwin, olap, sfax, midval, method, ...
+        scale, plt);
+    
     % save figure
-    savefile = strcat(mfilename, '_', replace(string(dt(ii)), ':', ...
-        '_'),'.eps');
-    figdisp(savefile, [], [], 2, [], 'epstopdf');
+    if strcmp(option, 'plot')
+        savefile = strcat(mfilename, '_', replace(string(dt(ii)), ':', ...
+            '_'),'.eps');
+        figdisp(savefile, [], [], 2, [], 'epstopdf');
     
     % write data
-    fid = fopen(strcat(savedir,mfilename,'_',titles{ii},'.txt'),'w');
-    % the columns: F     median   std   SwU(95%)     SwL(5%)
-    data = [F, Swmid, Swstd, SwU, SwL]';
-    fprintf(fid, '%10.6f %10.6f %10.6f %10.6f %10.6f\n', data);
-    fclose(fid);
+    else
+        fid = fopen(strcat(savedir,mfilename,'_',titles{ii},'.txt'),'w');
+        % the columns: F     median   std   SwU(95%)     SwL(5%)
+        data = [F, Swmid, Swstd, SwU, SwL]';
+        fprintf(fid, '%10.6f %10.6f %10.6f %10.6f %10.6f\n', data);
+        fclose(fid);
+    end
 end
 end
--- specdensplot_weekly.m
+++ specdensplot_weekly.m
@@ -1,20 +1,33 @@
-function specdensplot_weekly(win)
-% SPECDENSPLOT_WEEKLY()
+function specdensplot_weekly(win,option)
+% SPECDENSPLOT_WEEKLY(win, option)
 % makes spectral density of each week from Sep 2018 to Aug 2019 of ocean
 % noise recorded by a MERMAID P023
 %
 % INPUT
 % win       Length of the window in seconds
+% option    'plot' or 'save'
+%           'plot' gives log-scale spectral density heat map
+%           'save' gives SD profiles saved at ...
+%           /Users/sirawich/research/processed_data/weekly_SD_profiles/
 %
 % OUTPUT
-% figures saved at $EPS and
-% SD profiles saved at ...
+% figures saved at $EPS or SD profiles saved at ...
 % /Users/sirawich/research/processed_data/weekly_SD_profiles/
 %
 % SEE ALSO:
-% SPECDENSPLOT_SECTION
+% SPECDENSPLOT_SECTION, SPECDENSPLOT_HEATMAP
 % 
-% Last modified by Sirawich Pipatprathanporn: 07/29/2020
+% Last modified by Sirawich Pipatprathanporn: 08/03/2020
+
+defval('win', 100)
+defval('option', 'plot')
+
+% check invalid input
+if win <= 0
+    error('win must be above 0.');
+elseif ~or(strcmp(option, 'plot'), strcmp(option, 'save'))
+    error('option must be either ''plot'' or ''save''');
+end
 
 % dt_begin and dt_end for specdensplot_section
 dt_0 = datetime(2018, 9, 13, 'TimeZone', 'UTC', 'Format', ...
@@ -30,8 +43,13 @@ olap = 70;
 sfax = 10;
 midval = 'median';
 method = 'pct';
-scale = 'linear';
-plt = true;
+if strcmp(option, 'save')
+    scale = 'log';
+    plt = false;
+else
+    scale = 'linear';
+    plt = true;
+end
 
 savedir = '/Users/sirawich/research/processed_data/weekly_SD_profiles/';
 
@@ -40,16 +58,18 @@ for ii = 1:49
         specdensplot_section(dt(ii), dt(ii+1), excdir, nfft, fs, lwin, ...
         olap, sfax, midval, method, scale, plt);
     % save figure
-    savefile = strcat(mfilename, '_', replace(string(dt(ii)), ':', ...
-        '_'),'.eps');
-    figdisp(savefile, [], [], 2, [], 'epstopdf');
-    
+    if strcmp(option, 'plot')
+        savefile = strcat(mfilename, '_', replace(string(dt(ii)), ':', ...
+            '_'),'.eps');
+        figdisp(savefile, [], [], 2, [], 'epstopdf');
     % write data
-    titles = sprintf('%d_%02d_%02d',dt(ii).Year,dt(ii).Month,dt(ii).Day);
-    fid = fopen(strcat(savedir,mfilename,'_',titles,'.txt'),'w');
-    % the columns: F     median   std   SwU(95%)     SwL(5%)
-    data = [F, Swmid, Swstd, SwU, SwL]';
-    fprintf(fid, '%10.6f %10.6f %10.6f %10.6f %10.6f\n', data);
-    fclose(fid);
+    else
+        titles = sprintf('%d_%02d_%02d',dt(ii).Year,dt(ii).Month,dt(ii).Day);
+        fid = fopen(strcat(savedir,mfilename,'_',titles,'.txt'),'w');
+        % the columns: F     median   std   SwU(95%)     SwL(5%)
+        data = [F, Swmid, Swstd, SwU, SwL]';
+        fprintf(fid, '%10.6f %10.6f %10.6f %10.6f %10.6f\n', data);
+        fclose(fid);
+    end
 end
 end
