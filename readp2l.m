88701c4a527f6e80d0d2e7da44a1c5e850bbf062
Sirawich Pipatprathanporn <sirawich@princeton.edu>
2020-08-03 22:50:18 +0700

    Reads data from P2LFILE

---
+++ readp2l.m
@@ -0,0 +1,26 @@
+function [lon,lat,f,t,dt,p2l] = readp2l(p2lfile)
+% [lon,lat,f,t,p2l] = READP2L(p2lfile)
+% Reads data from a P2LFILE
+%
+% INPUT
+% p2lfile       full filename
+%
+% OUTPUT
+% lon           longitudes
+% lat           latitudes
+% f             frequencies
+% t             number of days since 1990-01-01
+% dt            datetimes
+% p2l           wave spectral density
+%               size(p2l) == [size(lon,1) size(lat,1) size(f,1) size(t,1)]
+%
+% Last modified by Sirawich Pipatprathanporn: 08/01/2020
+
+lon = ncread(p2lfile, 'longitude');
+lat = ncread(p2lfile, 'latitude');
+f = ncread(p2lfile, 'f');
+t = ncread(p2lfile, 'time');
+dt = t + datetime(1990, 1, 1, 0, 0, 0, 'Format', ...
+    'uuuu-MM-dd''T''HH:mm:ss.SSSSSS');
+p2l = ncread(p2lfile, 'p2l');
+end
