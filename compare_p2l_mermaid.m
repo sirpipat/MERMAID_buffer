c3cc32e4b529771d92144acc1874a5e00c4ae514
Sirawich Pipatprathanporn <sirawich@princeton.edu>
2020-08-05 21:28:35 +0700

    Added options to compare and plot either weekly or monthly spectral densities

--- compare_p2l_mermaid.m
+++ compare_p2l_mermaid.m
@@ -1,38 +1,50 @@
-function compare_p2l_mermaid()
-% COMPARE_P2L_MERMAID()
+function compare_p2l_mermaid(option)
+% COMPARE_P2L_MERMAID(option)
 % compares spectral density of WAVEWATCH surface equivalent pressure and 
 % spectral density of 1500-m depth pressure recorded by MERMAID.
 %
 % INPUT
-% no input
+% option        1 - weekly
+%               2 - monthly
 %
 % OUTOUT
 % no output beside figures saved at $EPS
 % 
-% Last modified by Sirawich Pipatprathanporn: 08/03/2020
+% Last modified by Sirawich Pipatprathanporn: 08/05/2020
 
 % WAVEWATCH spectral density files
-WWdir = '/Users/sirawich/research/processed_data/monthly_WWSD_profiles/';
-[allp2ls, pndex] = allfile(WWdir);
-
 % MERMAID spectral density files
-SDdir = '/Users/sirawich/research/processed_data/monthly_SD_profiles/';
+if option == 1
+    WWdir = '/Users/sirawich/research/processed_data/weekly_WWSD_profiles/';
+    SDdir = '/Users/sirawich/research/processed_data/weekly_SD_profiles/';
+else
+    WWdir = '/Users/sirawich/research/processed_data/monthly_WWSD_profiles/';
+    SDdir = '/Users/sirawich/research/processed_data/monthly_SD_profiles/';
+end
+
+[allp2ls, pndex] = allfile(WWdir);
 [allSDs, ~] = allfile(SDdir);
 
-% titles of specdensplot heatmap
-titles = {'September 2018', 'October 2018', 'November 2018', ...
-          'December 2018', 'January 2019', 'February 2019', ...
-          'March 2019', 'April 2019', 'May 2019', ...
-          'June 2019', 'July 2019', 'August 2019'};
-      
-save_titles = {'2018_09', '2018_10', '2018_11', '2018_12', '2019_01', ...
-               '2019_02', '2019_03', '2019_04', '2019_05', '2019_06', ...
-               '2019_07', '2019_08'};
-           
-% create figure
-figure(1);
-clf;
-set(gcf, 'Unit', 'inches', 'Position', [2 2 15 13]);
+% titles of specdensplot
+if option == 1
+    dt_0 = datetime(2018, 9, 13, 'TimeZone', 'UTC', 'Format', ...
+        'uuuu_MM_dd');
+    dt_week = dt_0 + calweeks(0:48);
+    save_titles = string(dt_week);
+    titles = replace(save_titles, '_', '-');
+else
+    titles = {'September 2018', 'October 2018', 'November 2018', ...
+              'December 2018', 'January 2019', 'February 2019', ...
+              'March 2019', 'April 2019', 'May 2019', ...
+              'June 2019', 'July 2019', 'August 2019'};
+
+    save_titles = {'2018_09', '2018_10', '2018_11', '2018_12', '2019_01', ...
+                   '2019_02', '2019_03', '2019_04', '2019_05', '2019_06', ...
+                   '2019_07', '2019_08'};
+end
+
+y_shifts = zeros(1,pndex);
+f_scales = zeros(1,pndex);
 for ii = 1:pndex
     % read monthly WAVEWATCH spectral density
     fid = fopen(allp2ls{ii},'r');
@@ -66,12 +78,18 @@ for ii = 1:pndex
     m_f_highest = sum(m_fp .* m_sdp) / sum(m_sdp);
     f_scale = m_f_highest / f_highest;
     
-    % grid coordinates for panels (1,1) = bottom left, (4,3) = top right
-    yy = 3 - ceil(ii/4);
-    xx = ii - 4 * ceil(ii/4) + 3;
-    ax = subplot('Position',[xx/4+0.04 yy/3+0.05 1/4-0.07 1/3-0.1]);
+    y_shifts(1,ii) = y_shift;
+    f_scales(1,ii) = f_scale;
+    
+    y_shift = 0;
+    f_scale = 1;
+    
+    % create figure
+    figure(1);
+    clf;
+    set(gcf, 'Unit', 'inches', 'Position', [2 2 6.5 6.5]);
     % plot
-    ylim([20 140]);
+    ax = subplot('Position',[0.1 0.09 0.85 0.8]);
     p1 = semilogx(f * f_scale, sd + y_shift, '^-k', 'MarkerFaceColor', 'k');
     hold on
     p2 = semilogx(f * f_scale, sdU + y_shift, '^-', 'Color', ...
@@ -86,6 +104,7 @@ for ii = 1:pndex
     xlim([0.01 2]);
     xlabel('frequency (Hz)');
     ylabel('10 log_{10} spectral density');
+    ylim([-20 140]);
     legend([p1 p4], {'WAVEWATCH', 'MERMAID'}, 'Location', 'southeast')
     ax.TickDir = 'both';
     
@@ -95,11 +114,11 @@ for ii = 1:pndex
     title(ax2,sprintf('%s (f-scale = %.3f, SD-shift = %.3f)', titles{ii}, ...
         f_scale, y_shift));
     
-%     % save figure
-%     figdisp(strcat(mfilename,'_',save_titles{ii},'.eps'),...
-%         [],[],2,[],'epstopdf');
+    % save figure
+    figdisp(strcat(mfilename,'_',save_titles{ii},'.eps'),...
+        [],[],2,[],'epstopdf');
 end
 
-% save figure
-figdisp(strcat(mfilename,'_one_year.eps'),[],[],2,[],'epstopdf');
+fprintf('SD-shift = %.4f\n', mean(y_shifts));
+fprintf('f-scale  = %.4f\n', mean(f_scales));
 end
