a91c66c478c45d2f5da9e272b5907f63e1020939
Sirawich Pipatprathanporn <sirawich@princeton.edu>
2020-08-31 10:01:22 +0700

    Allowed to read multiple tags

--- readeventlist.m
+++ readeventlist.m
@@ -11,20 +11,31 @@ fclose(fid);
 data = split(txt);
 data = data(1:end-1);
 data = reshape(data,4,size(data,1)/4)';
-begins = datetime(data(:,1),'InputFormat',fmt,'TimeZone','UTC',...
+data_begins = datetime(data(:,1),'InputFormat',fmt,'TimeZone','UTC',...
     'Format',fmt);
-ends = datetime(data(:,2),'InputFormat',fmt,'TimeZone','UTC',...
+data_ends = datetime(data(:,2),'InputFormat',fmt,'TimeZone','UTC',...
     'Format',fmt);
-arrivals = datetime(data(:,3),'InputFormat',fmt,'TimeZone','UTC',...
+data_arrivals = datetime(data(:,3),'InputFormat',fmt,'TimeZone','UTC',...
     'Format',fmt);
-tags = data(:,4);
+data_tags = data(:,4);
 
-if size(tag,1) == 1
-    begins = begins(strcmp(tags,tag));
-    ends = ends(strcmp(tags,tag));
-    arrivals = arrivals(strcmp(tags,tag));
-    tags = tags(strcmp(tags,tag));
-else
+if size(tag,1) >= 1
+    begins = [];
+    ends = [];
+    arrivals = [];
+    tags = [];
+    for ii = 1:size(tag,1)
+        begins = [begins; data_begins(strcmp(data_tags,tag{ii}))];
+        ends = [ends; data_ends(strcmp(data_tags,tag{ii}))];
+        arrivals = [arrivals; data_arrivals(strcmp(data_tags,tag{ii}))];
+        tags = [tags; data_tags(strcmp(data_tags,tag{ii}))];
+    end
+end
+
+[begins, sorted_index] = sort(begins);
+ends = ends(sorted_index);
+arrivals = arrivals(sorted_index);
+tags = tags(sorted_index);
 
 % figure(1);
 % clf
