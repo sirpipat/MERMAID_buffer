4b2d9827e07966d918aaaa40d08ec6771e8dbf9f
Sirawich Pipatprathanporn <sirawich@princeton.edu>
2020-09-01 13:48:18 +0700

    Added SS phase and distance to the output struct, and changed time criteria

--- findevents.m
+++ findevents.m
@@ -1,9 +1,9 @@
-function events = findevents(arrival,stlo,stla)
+function evs = findevents(arrival,stlo,stla)
 % events = FINDEVENTS(arrival,stlo,stla)
 % Search for event(s) that produce(s) signal in the seismogram. It requests
 % events information from the catalog on IRIS. Then, it computes the
-% theoretical arrival time using ray theory. It returns events which the
-% P-wave arrives within 3 minutes from the actual arrival time.
+% theoretical arrival time using ray theory. It returns events that arrive
+% within 3 minutes from the actual arrival time.
 %
 % INPUT
 % arrival       arrival time from the seismogram
@@ -11,13 +11,13 @@ function events = findevents(arrival,stlo,stla)
 % stla          station latitude
 %
 % OUTPUT
-% events        the list of events which P-wave arrives with in 3 minutes
+% evs           the list of events which P-wave arrives with in 3 minutes
 %               from arrival
 %
 % SEE ALSO
 % IRISFETCH, TAUPTIME
 %
-% Last modified by Sirawich Pipatprathanporn: 08/25/2020
+% Last modified by Sirawich Pipatprathanporn: 09/01/2020
 
 defval('stlo',[]);
 defval('stla',[]);
@@ -29,25 +29,43 @@ end
 arrival.Format = 'uuuu-MM-dd HH:mm:ss.SSSSSS';
 ev = irisFetch.Events('starttime', string(arrival - minutes(20)), ...
     'endtime', string(arrival + minutes(1)));
+arrival.Format = 'uuuu-MM-dd''T''HH:mm:ss.SSSSSS';
 
-phases = 'p, P, pP, PP, Pn, Pg, s, S, Sn, Sg, PcP, Pdiff, PKP, PKiKP, PKIKP';
+phases = 'p, P, pP, PP, Pn, Pg, s, S, Sn, Sg, SS, PcP, Pdiff, PKP, PKiKP, PKIKP';
 
-events = [];
+evs = [];
 for ii = 1:size(ev,2)
     depth = max(0, ev(ii).PreferredDepth);
     t = taupTime('ak135',depth,phases,'sta',[stla stlo], ...
         'evt',[ev(ii).PreferredLatitude ev(ii).PreferredLongitude]);
     if ~isempty(t)
-        expected_arrival = datetime(ev(ii).PreferredTime,'TimeZone','UTC',...
-            'Format','uuuu-MM-dd HH:mm:ss.SSSSSS') + seconds(t(1).time);
-        diff = expected_arrival - arrival;
-        if abs(diff) < minutes(3)
-            events = [events; ev(ii)];
+        ev(ii).phase = cell(0);
+        ev(ii).travelTime = [];
+        ev(ii).expArrivalTime = [];
+        ev(ii).diff = [];
+        ev(ii).stlo = stlo;
+        ev(ii).stla = stla;
+        ev(ii).evlo = ev(ii).PreferredLongitude;
+        ev(ii).evla = ev(ii).PreferredLatitude;
+        ev(ii).distance = t(1).distance;
+        for jj = 1:size(t,2)
+            expected_arrival = datetime(ev(ii).PreferredTime,'TimeZone','UTC',...
+                'Format','uuuu-MM-dd''T''HH:mm:ss.SSSSSS') + seconds(t(jj).time);
+            diff = expected_arrival - arrival;
+            ev(ii).phase{size(ev(ii).phase,2)+1} = t(jj).phaseName;
+            ev(ii).travelTime = [ev(ii).travelTime t(jj).time];
+            ev(ii).expArrivalTime = [ev(ii).expArrivalTime ...
+                expected_arrival];
+            ev(ii).diff = [ev(ii).diff diff];
             ev(ii)
             fprintf('Arrival Time : %s\n',string(arrival));
             fprintf('Expected Time: %s\n',string(expected_arrival));
             fprintf('Difference   : %s\n',string(diff));
         end
     end
+    if min(abs(ev(ii).diff)) < minutes(3)
+        evs = [evs; ev(ii)];
+    end
 end
+
 end
