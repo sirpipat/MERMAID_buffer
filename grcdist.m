c7e54051d2bb829b6db9c96027778d6e2cd0f83d
Sirawich Pipatprathanporn <sirawich@princeton.edu>
2020-09-24 11:44:39 +0700

    Documentation

--- grcdist.m
+++ grcdist.m
@@ -1,4 +1,16 @@
 function [distKM, distDeg] = grcdist(lon1lat1, lon2lat2)
+% [distKM, distDeg] = GRCDIST([lon1 lat1], [lon2 lat2])
+% Calculates the distance between two points on a great circle.
+%
+% INPUT:
+% [lon1 lat1]       longitude and latitude of the starting point (degrees)
+% [lon2 lat2]       longitude and latitude of the ending point (degrees)
+%
+% OUTPUT:
+% distKM            distance in kilometers
+% distDeg           distance in degrees
+%
+% Last modified by Sirawich Pipatprathanporn: 09/15/2020
 
 % Conversion to radians
 lon1lat1=lon1lat1 * pi / 180;
