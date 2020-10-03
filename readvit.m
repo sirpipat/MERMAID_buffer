c1352a22e4508e7ec73a99e830bba98638455bc4
Sirawich Pipatprathanporn <sirawich@princeton.edu>
2020-10-03 15:14:51 +0700

    Moved all vit file reading process to readvit.m

--- readvit.m
+++ readvit.m
@@ -1,12 +1,55 @@
-function text = readvit(file)
-% text = readvit(file)
+function T = readvit(fname)
+% T = readvit(fname)
 % 
 % Load a .vit file
 % 
 % INPUT:
-% file = filename string
+% fname     full filename of the .vit file
 % 
-% last modified by Sirawich Pipatprathanporn, 10/07/2019
+% OUTPUT:
+% T         table from .vit file with the following fields
+%             'Station'
+%             'Date'        datetime with hh:mm:ss == 00:00:00
+%             'Time'
+%             'stla'
+%             'stlo'
+%             'HDil'
+%             'VDil'
+%             'VBat'
+%             'Vmin'
+%             'PInt'
+%             'PExt'
+%             'PRange'
+%             'NumCommand'
+%             'NumQueued'
+%             'NumUploaded'
+%
+% Example
+% % read P023 vit file [default]
+% T = readvit();
+% dt = T.Date + T.Time;
+%
+% Last modified by Sirawich Pipatprathanporn, 10/01/2020
 
-x = loadb(file, '*char', 'l');
-text = convertCharsToStrings(x);
+defval('fname','/Users/sirawich/research/raw_data/metadata/P023_all.txt')
+
+% read vit file
+opts = detectImportOptions(fname);
+T = readtable(fname,opts);
+T.Properties.VariableNames = {'Station', ...
+                              'Date', ...
+                              'Time', ...
+                              'stla', ...
+                              'stlo', ...
+                              'HDil', ...
+                              'VDil', ...
+                              'VBat', ...
+                              'Vmin', ...
+                              'PInt', ...
+                              'PExt', ...
+                              'PRange', ...
+                              'NumCommand', ...
+                              'NumQueued', ...
+                              'NumUploaded'};
+                          
+end
