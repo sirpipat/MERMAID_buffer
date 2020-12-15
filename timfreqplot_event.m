d7f64f922485b575d42420e7d7172353fd5bde29
Sirawich Pipatprathanporn <sirawich@princeton.edu>
2020-12-15 18:57:11 +0700

    Same as timfreqplot but the time is from the origin time of an event

---
+++ timfreqplot_event.m
@@ -0,0 +1,80 @@
+function fig = timfreqplot_event(ev, dt_begin)
+% fig = TIMFREQPLOT_EVENT(ev, dt_begin)
+%
+% plot seismogram, power spectral density, spectograms, and filtered
+% seismogram but the time is from the origin time of an event
+%
+% INPUT
+% ev            event
+% dt_begin      begining datetime of a section to plot
+%
+% OUTPUT
+% fig           figure handling the plots
+% 
+% SEE ALSO
+% TIMFREQPLOT
+%
+% Last modified by Sirawich Pipatprathanporn, 12/15/2020
+
+% reference spectral density files
+SDdir = '/Users/sirawich/research/processed_data/monthly_SD_profiles_before_conversion/';
+[allSDs,dndex] = allfile(SDdir);
+
+% input parameters for timfreqplot
+fs = 40.01406;
+nfft = round(100 * fs);
+lwin = nfft;
+olap = 70;
+sfax = 10;
+beg = 0;
+unit = 's';
+p = [];
+
+% read the neccessary part of the buffer
+[sections, intervals] = getsections(getenv('ONEYEAR'), dt_begin, dt_begin + hours(1), fs);
+[y, dt_begin, dt_end] = readsection(sections{1}, intervals{1}{1}, intervals{1}{2}, fs);
+
+% plot the spectrogram, power spectral density plot, and seismograms
+fig = timfreqplot(y, [], [], [], [], dt_begin, nfft, fs, lwin, olap, ...
+    sfax, beg, unit, p, false, false);
+
+% make time axis relative to the origin time
+dt_origin = datetime(ev.Origins.Time, 'TimeZone', 'UTC', 'Format', ...
+    'uuuu-MM-dd''T''HH:mm:ss.SSSSSS');
+time_labels = string(duration((0:5) * minutes(15), 'Format', 'hh:mm'));
+% make time axis relative to the origin time for seismograms
+fig.Children(1).XTick = dt_origin + (0:5) * minutes(15);
+fig.Children(2).XTick = dt_origin + (0:5) * minutes(15);
+fig.Children(3).XTick = dt_origin + (0:5) * minutes(15);
+fig.Children(1).XTickLabel = time_labels;
+fig.Children(1).XLabel.String = 'time since origin (hh:mm)';
+% make time axis relative to the origin time for spectrogram
+seconds_since_origin = seconds(dt_origin + (0:5) * minutes(15) - dt_begin);
+fig.Children(7).XTick = seconds_since_origin;
+fig.Children(7).XTickLabel = time_labels;
+fig.Children(7).XLabel.String = 'time since origin (hh:mm): 100 s window';
+fig.Children(8).XTick = seconds_since_origin;
+fig.Children(8).XTickLabel = time_labels;
+
+% add the expected arrival time
+% TODO?
+
+% read spectral density refernce data
+% September 2018 --> 1, August 2019 --> 12
+index = (dt_begin.Year - 2018) * 12 + dt_begin.Month - 8;
+fid = fopen(allSDs{index},'r');
+data = fscanf(fid,'%f %f %f %f %f',[5 Inf]);
+fclose(fid);
+% add reference spectral density of the month
+ax = fig.Children(4);
+axes(ax)
+hold on
+semilogx(data(1,:),data(2,:),'Color',rgbcolor('deep sky blue'));
+semilogx(data(1,:),data(4,:),'Color',rgbcolor('gray'));
+semilogx(data(1,:),data(5,:),'Color',rgbcolor('gray'));
+hold off
+
+% save figure
+filename = strcat(mfilename, '_', replace(string(dt_begin), ':', '_'), '.eps');
+figdisp(filename,[],[],2,[],'epstopdf');
+end
