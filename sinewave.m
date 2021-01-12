6300e93c5cf337f9fc67efa3b7234f88a3887de5
Sirawich Pipatprathanporn <sirawich@princeton.edu>
2021-01-12 19:13:40 +0700

    It will no longer normalize the sinewave

--- sinewave.m
+++ sinewave.m
@@ -13,7 +13,7 @@ function [t, x] = sinewave(dt, amps, freqs, phases, fs)
 % t         equally-spaced time with sampling frequency of fs
 % x         sine waves signal
 %
-% Last modified by Sirawich Pipatprathanporn: 01/07/2021
+% Last modified by Sirawich Pipatprathanporn: 01/12/2021
 
 % convert all inputs to row vectors
 if size(amps, 2) > 1
@@ -29,7 +29,4 @@ end
 % creates sine functions
 t = 0:(1/fs):dt;
 x = amps' * sin(2 * pi * (freqs * t) + phases);
-
-% normalizes the funciton
-x = x / length(freqs);
 end
