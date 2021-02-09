b512837acb83c894a46367ddb9e530352be3fe8e
Sirawich Pipatprathanporn <sirawich@princeton.edu>
2021-02-09 21:52:01 +0700

    Changed a varible name 'cell' to 'cells' to avoid name collision

--- getfndex.m
+++ getfndex.m
@@ -1,38 +1,38 @@
-function fndex = getfndex(cell, value)
-% index = GETFNDEX(cell, value)
+function fndex = getfndex(cells, value)
+% index = GETFNDEX(cells, value)
 %
 % INPUT:
-% cell      sorted cell
+% cells     sorted cell
 % value     value of interest, must be the same datatype as array's members
 %
 % OUTPUT:
-% fndex     index of the cell{index} closest to the value
+% fndex     index of the cells{index} closest to the value
 %
 % Last modified by Sirawich Pipatprathanporn: 02/09/2021
 
-a = size(cell);
+a = size(cells);
 a_size = a(2);
 high_bound = a_size;
 low_bound = 1;
 curr_index = middle(high_bound, low_bound);
 
 % check if the value is beyond the limit of the array
-if value < cell{1}
+if value < cells{1}
     fndex = 1;
     return
-elseif value > cell{end}
-    fndex = length(cell);
+elseif value > cells{end}
+    fndex = length(cells);
     return
 end
 
-while value ~= cell{curr_index}
+while value ~= cells{curr_index}
     % check the high_bound and low_bound if there is nothing between
     if high_bound - low_bound <= 1
         fndex = low_bound;
         return
     end
     % try to shrink the range between high_bound and low_bound
-    if value > cell{curr_index}
+    if value > cells{curr_index}
         low_bound = curr_index;
         curr_index = middle(high_bound, low_bound);
     else
