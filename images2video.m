1a32b3a66ff6abdce94c7009ed74b73d69532ac4
Sirawich Pipatprathanporn <sirawich@princeton.edu>
2021-06-17 23:33:11 +0700

    Documentation and added savedir

--- images2video.m
+++ images2video.m
@@ -1,8 +1,25 @@
-function v = images2video(images, ext, framerate, savename)
+function v = images2video(images, ext, framerate, savename, savedir)
+% v = IMAGES2VIDEO(images, ext, framerate, savename)
+%
+% Convert a series of photos to video.
+%
+% INPUT
+% images        a series of images
+% ext           file extension of the images
+% framerate     framerate of the video (frames per second)
+% savename      name of the video
+% savedir       directory of the saved video
+%               The video is saved as 'images2video_savename.mp4'
+%
+% OUTPUT
+% v             video object
+%
+% Last modified by Sirawich Pipatprathanporn, 06/09/2021
 
+defval('savedir', getenv('EPS'))
 
 % create the video writer
-v = VideoWriter(strcat(pwd, '/', mfilename, '_', savename, '.mp4'), 'MPEG-4');
+v = VideoWriter(strcat(savedir, '/', mfilename, '_', savename, '.mp4'), 'MPEG-4');
 v.FrameRate = framerate;
 
 % open the video writer
