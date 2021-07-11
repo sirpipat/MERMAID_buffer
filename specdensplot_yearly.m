c2146c5e16895a86340460c5b67c0375b4d53883
Sirawich Pipatprathanporn <sirawich@princeton.edu>
2021-07-11 20:45:16 +0700

    Brought panels closer

--- specdensplot_yearly.m
+++ specdensplot_yearly.m
@@ -14,7 +14,7 @@ function specdensplot_yearly(win)
 % SEE ALSO:
 % SPECDENSPLOT_SECTION
 % 
-% Last modified by Sirawich Pipatprathanporn: 07/02/2021
+% Last modified by Sirawich Pipatprathanporn: 07/11/2021
 
 % dt_begin and dt_end for specdensplot_section
 dt_0 = datetime(2018, 9, 1, 'TimeZone', 'UTC', 'Format', ...
@@ -31,7 +31,7 @@ sfax = 10;
 midval = 'median';
 method = 'pct';
 scale = 'log';
-rmtransfer = true;
+rmtransfer = false;
 plt = false;
 
 % titles of specdensplot heatmap
@@ -48,8 +48,8 @@ clf
 [~, up, np, F, SDbins, Swcounts, Swmean, ~, SwU, SwL] = ...
     specdensplot_section(dt(1), dt(2), excdir, nfft, fs, lwin, olap, ...
     sfax, midval, method, scale, rmtransfer, plt);
-ax = subplot('Position',[0.05 2/3+0.05 1/4-0.07 1/3-0.1]);
-[ax,axs,axb] = specdensplot_heatmap(ax, up, np, F, SDbins, Swcounts, Swmean, SwU, ...
+ax(1) = subplot('Position',[0.05 2/3+0.05 1/4-0.07 1/3-0.1]);
+[ax(1),axs(1),axb(1)] = specdensplot_heatmap(ax(1), up, np, F, SDbins, Swcounts, Swmean, SwU, ...
     SwL, sfax, scale, titles{1});
 
 Swcounts_total = Swcounts;
@@ -62,9 +62,34 @@ for ii = 2:12
     % grid coordinates for panels (1,1) = bottom left, (4,3) = top right
     yy = 3 - ceil(ii/4);
     xx = ii - 4 * ceil(ii/4) + 3;
-    ax = subplot('Position',[xx/4+0.05 yy/3+0.05 1/4-0.07 1/3-0.1]);
-    [ax,axs,axb] = specdensplot_heatmap(ax, up, np, F, SDbins, Swcounts, Swmean, ...
+    ax(ii) = subplot('Position',[xx/4+0.05 yy/3+0.05 1/4-0.07 1/3-0.1]);
+    [ax(ii),axs(ii),axb(ii)] = specdensplot_heatmap(ax(ii), up, np, F, SDbins, Swcounts, Swmean, ...
         SwU, SwL, sfax, scale, titles{ii});
 end
+
+% Make a loop for this
+for ii = [2 3 4 6 7 8 10 11 12]
+    ax(ii).YLabel = [];
+    ax(ii).YTickLabel = [];
+end
+
+% another loop
+for ii = [1 2 3 5 6 7 9 10 11]
+    axs(ii).YTickLabel = [];
+end
+
+% bring panels closer
+for ii = 0:4:8
+    serre(ax((1:4) + ii), 0.7, 'across');
+    serre(axs((1:4) + ii), 0.7, 'across');
+    serre(axb((1:4) + ii), 0.27,'across');
+end
+
+for jj = 0:3
+    serre(ax([1 5 9] + jj),0.2,'down');
+    serre(axs([1 5 9] + jj),0.2,'down');
+    serre(axb([1 5 9] + jj),0.121,'down');
+end
+
 figdisp('yearly_summary', [], [], 2, [], 'epstopdf');
 end
