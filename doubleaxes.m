219bf1dddf7a796da7380e2b61769927a0af41f5
Sirawich Pipatprathanporn <sirawich@princeton.edu>
2021-09-14 23:09:05 -0400

    Axis can be datetime and duration

--- doubleaxes.m
+++ doubleaxes.m
@@ -10,12 +10,35 @@ function ax2 = doubleaxes(ax)
 % OUTPUT
 % ax2   the second axes
 %
-% Last modified by Sirawich Pipatprathanporn: 11/10/2020
+% Last modified by Sirawich Pipatprathanporn: 09/14/2021
 
 ax2 = axes();
 ax2.Position = ax.Position;
 ax2.DataAspectRatio = ax.DataAspectRatio;
 ax2.DataAspectRatioMode = ax.DataAspectRatioMode;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Switches the second axes axis rulers to datetime or duration if necessary
+% Otherwise, setting limits to the axes will yield an error.
+xlimits = ax.XAxis.Limits;
+ylimits = ax.YAxis.Limits;
+if isdatetime(xlimits)
+    xval = datetime('today', 'TimeZone', 'UTC');
+elseif isduration(xlimits)
+    xval = seconds(1);
+else
+    xval = 1;
+end
+if isdatetime(ylimits)
+    yval = datetime('today', 'TimeZone', 'UTC');
+elseif isduration(ylimits)
+    yval = seconds(1);    
+else
+    yval = 1;
+end
+plot(xval, yval);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 ax2.XAxisLocation = 'top';
 ax2.FontSize = ax.FontSize;
 ax2.XScale = ax.XScale;
