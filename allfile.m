1eab5beb974e3446291285ffc413104652909236
Sirawich Pipatprathanporn <sirawich@princeton.edu>
2021-11-22 12:19:25 -0500

    Handles non-existing or empty directories

--- allfile.m
+++ allfile.m
@@ -10,8 +10,10 @@ function [allfiles,fndex] = allfile(dir)
 %
 % allfiles   Bottom-file list with complete file names
 % fndex      The total number of elements in the list
+%            -1 if dir does not exist as a directory
+%             0 if dir is an empty directory
 %
-% Last modified by Sirawich Pipatprathanporn, 02/05/2020
+% Last modified by Sirawich Pipatprathanporn, 11/01/2021
 %
 % Remember that when using LS2CELL in full path mode, you need the
 % trailing file separators
@@ -19,7 +21,28 @@ function [allfiles,fndex] = allfile(dir)
 % Makes the table of contents
 allfiles = {};
 fndex = 0;
-files = ls2cell(dir, 1);
+
+% check whether dir is a directory
+switch exist(dir)
+    case 2
+        % fprintf('It exists as a file not a directory.\n');
+        fndex = -1;
+        return
+    case 7
+    otherwise
+        % fprintf('This directory does not exist');
+        fndex = -1;
+        return
+end
+
+% check whether dir is empty
+try
+    files = ls2cell(dir, 1);
+catch
+    % fprintf('This direcotry is empty.\n');
+    return
+end
+
 for index = 1:length(files)
     allfiles{index} = files{index};
 end
