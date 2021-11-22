992724954a085fa8094f591e62770d7ab6b3c267
Sirawich Pipatprathanporn <sirawich@princeton.edu>
2021-11-22 12:54:05 -0500

    Changed datetime's time zone to 'UTC'

--- readp2l.m
+++ readp2l.m
@@ -14,13 +14,13 @@ function [lon,lat,f,t,dt,p2l] = readp2l(p2lfile)
 % p2l           wave spectral density
 %               size(p2l) == [size(lon,1) size(lat,1) size(f,1) size(t,1)]
 %
-% Last modified by sirawich-at-princeton.edu: 11/17/2021
+% Last modified by sirawich-at-princeton.edu: 11/22/2021
 
 lon = ncread(p2lfile, 'longitude');
 lat = ncread(p2lfile, 'latitude');
 f = ncread(p2lfile, 'f');
 t = ncread(p2lfile, 'time');
 dt = t + datetime(1990, 1, 1, 0, 0, 0, 'Format', ...
-    'uuuu-MM-dd''T''HH:mm:ss.SSSSSS');
+    'uuuu-MM-dd''T''HH:mm:ss.SSSSSS', 'TimeZone', 'UTC');
 p2l = ncread(p2lfile, 'p2l');
 end
