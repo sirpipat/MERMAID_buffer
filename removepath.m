48a98ae9affc8baf037212a5690639b139c6332e
Sirawich Pipatprathanporn <sirawich@princeton.edu>
2024-09-18 16:30:38 -0400

    Use filesep to detect the directory separator, so PC users may use it

--- removepath.m
+++ removepath.m
@@ -1,9 +1,26 @@
 function filename = removepath(full_filename)
-% filename = removepath(full_filename)
-% remove the path from filename string
-% e.g. remove_path('/home/Document/file.txt') == 'file.txt'
+% filename = REMOVEPATH(full_filename)
+%
+% Removes the path from filename string
+%
+% INPUT:
+% full_filename         full path to a file name
+%
+% OUTPUT:
+% filename              file name without the path to the file
+%
+% EXAMPLE:
+% % returns 'file.txt', for non-PC machine
+% remove_path('/home/Document/file.txt')
+%
+% % example for PC
+% remove_path('\home\Document\file.txt')
+%
+% SEE ALSO:
+% FILEPARTS
+%
+% Last modified by sirawich-at-princeton.edu, 09/18/2024
 
-splited_name = split(full_filename, '/');
+splited_name = split(full_filename, filesep);
 filename = cell2mat(splited_name(end));
-
 end
